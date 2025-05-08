abb3a431a71be79edd91a074659d5e9eb6dc55cf
gsranawat99 <gsranawat99@gmail.com>
2025-05-08 23:57:51 +0530

    initial demos2

--- tripplanner/app.py
+++ tripplanner/app.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 import os
 from TravelAgents import guide_expert, location_expert, planner_expert, set_openai_api_key
